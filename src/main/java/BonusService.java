2cea55c47d665ab74887da426230ddf275f5ddcd
Alena Korovina <evtam-29@mail.ru>
2020-10-21 19:08:18 +0300

    fixed#2

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -7,6 +7,9 @@ public class BonusService {
             bonus = limit;
         }
         return bonus;
+        // long actual = service.calculate(amount, registered);
+        //        assertEquals(expected, actual);
+
     }
 }
 
